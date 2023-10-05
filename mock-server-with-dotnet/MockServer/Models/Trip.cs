623ca0079bc7247efe953c5118df1191457bd2da
Vlad Morosanu <vladmorosanu97@gmail.com>
2023-10-05 18:54:40 +0300

    Update mock server

---
+++ mock-server-with-dotnet/MockServer/Models/Trip.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace MockServer.Models
+{
+    public partial class Trip
+    {
+        public long Id { get; set; }
+
+        public Uri PhotoUrl { get; set; }
+
+        public string Title { get; set; }
+
+        public string Subtitle { get; set; }
+
+        public string[] Countries { get; set; }
+
+        public long Days { get; set; }
+
+        public double Co2Kilograms { get; set; }
+
+        public double Rating { get; set; }
+
+        public string Description { get; set; }
+
+        public Advantage[] Advantages { get; set; }
+    }
+}
